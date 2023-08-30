59e22e2b9cc3ba03638efe5e8da1aa7d7b1fe056
Rahmat ullah badsha <87254817+Tamhat@users.noreply.github.com>
2023-08-30 23:37:52 +0600

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 
 import "../globals.css";
 
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        //baseTheme: dark,
       }}
     >
       <html lang='en'>
